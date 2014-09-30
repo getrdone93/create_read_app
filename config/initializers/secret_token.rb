9333c9badf326ba440bcf884981df7d8e5a74480
Trever L. Anderson <treveranderson@unomaha.edu>
2014-09-29 21:18:37 -0500

    This is the first commit. Hopefully this works now.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CreateReadApp::Application.config.secret_key_base = 'b2a945d4f8b27e47f105575da6dc105a21c8d48356c18c789e71b868797a1534362fa4b28115534abb4aefce9b7c0b6e8121a87dbd71bf51bf62791e8c0a0a13'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CreateReadApp::Application.config.session_store :cookie_store, key: '_create_read_app_session'
